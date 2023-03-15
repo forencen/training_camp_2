e1c58d246853ef9129bec414882a26b3b6e2d809
hurongcheng <hurongcheng0205@gmail.com>
2023-03-15 22:41:34 +0800

    w2-1 bank

---
+++ w2-1/hardhat_bank/contracts/MyBank.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract MyBank {
+    mapping(address => uint256) public balances;
+    address public owner;
+
+    event UserReceive(address user, uint amount);
+
+    constructor () payable {
+        owner = msg.sender;
+        balances[owner] = msg.value;
+    }
+    receive() external payable {
+        balances[msg.sender] = balances[msg.sender] + msg.value;
+        emit UserReceive(msg.sender, msg.value);
+    }
+
+    function deposit() external payable{
+        balances[msg.sender] = balances[msg.sender] + msg.value;
+        emit UserReceive(msg.sender, msg.value);
+    }
+
+    function Withdraw(uint x) external{
+        require(balances[msg.sender] >= x, "user balance not enough");
+        balances[msg.sender] = balances[msg.sender] - x;
+        (bool flag,) = payable(msg.sender).call{value: x}("");
+        require(flag, "user withdraw failed");
+    }
+}
